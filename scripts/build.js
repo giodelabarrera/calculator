28fe817445edc55a192e917db4ab6dbb6c5b9f48
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-03-16 14:24:27 +0100

    Remove dist folder when run build

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 const { spawnSync } = require("./utils");
 
-if (!fs.existsSync("./dist")) {
-  spawnSync("mkdir -p ./dist");
-}
+if (fs.existsSync("./dist")) spawnSync("rm -r ./dist");
+spawnSync("mkdir -p ./dist");
 
 spawnSync("cp -r ./src/* ./dist");
 spawnSync("rm -r ./dist/__tests__");
