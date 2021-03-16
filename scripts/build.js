989bc55ea04b3b263b0a0bedf8fc5894cd6a9f5d
Giorgio de la Barrera <giodelabarrera@gmail.com>
2021-03-16 14:18:59 +0100

    Add build script

---
+++ scripts/build.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const { spawnSync } = require("./utils");
+
+if (!fs.existsSync("./dist")) {
+  spawnSync("mkdir -p ./dist");
+}
+
+spawnSync("cp -r ./src/* ./dist");
+spawnSync("rm -r ./dist/__tests__");
